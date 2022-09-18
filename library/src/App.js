5135580f3b53c481bea9231768da23b76b8232e0
Lenka <milosevic.lenka@gmail.com>
2022-09-18 14:00:47 +0200

    Instaliran react

---
+++ library/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App Component</h1>
+      <h2> sjcksj</h2>
+    </div>
+  );
+}
+
+export default App;
